2bf7b312b0b5d363797ce8ca293f6e96a25b31a1
Shelton Louis <louisshelton0@gmail.com>
2025-07-25 09:10:30 -0400

    Refactor grammar to use regex constants
    Use regex constants for improved readability and maintainability. This
    refactoring defines constants for commonly used regular expressions,
    such as space, newline, and various delimiters. This makes the grammar
    rules clearer and easier to modify.
    
    Make the rules testable by using and chioces for the source_file rule.

--- grammar.js
+++ grammar.js
@@ -7,17 +7,26 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+const UNLIMITED_SPACE = /\s+/;
+const NEWLINE = /\n/;
+const HEADING_MARKER = /[\=#]/;
+const BLOCK_DELIMITER = /(?:={4,}|-{4,}|\.{4,}|\*{4,}|\+{4,}|_{4,})/;
+const INLINE_FORMATTING_MARKER = /(?:\*|_|`|#|\+)/;
+const LINE_COMMENT_PREFIX = /\/\//;
+const ATTRIBUTE_LIST_START = /\[/;
+const ATTRIBUTE_LIST_END = /\]/;
+const DOC_ATTRIBUTE_DECLARATION_START = /:[a-zA-Z0-9_\-]+:/;
+
 module.exports = grammar({
   name: "asciidoc",
 
   rules: {
     // TODO: add the actual grammar rules
-    source_file: ($) => repeat($.document_title),
-    // space: ($) => /\s+/,
+    source_file: ($) => choice($.document_title),
     document_title: ($) =>
       seq(
-        field("marker", /[\=#]/),
-        /\s+/,
+        field("marker", token(HEADING_MARKER)),
+        UNLIMITED_SPACE,
         choice(
           field("title", /[\w\s]+/),
           seq(field("title", /[\w\s]+:/), field("subtitle", /[\w\s]+/)),
